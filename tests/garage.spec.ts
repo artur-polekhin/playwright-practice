8a1cdc9972760efe705d6b93bc041eef18c1c436
Ko4aH4uK <>
2026-03-21 22:15:08 +0200

    added hw_28.1

--- tests/garage.spec.ts
+++ tests/garage.spec.ts
@@ -1,57 +1,26 @@
-import test from "@playwright/test";
-import AddCarForm from "../pom/forms/AddCarForm";
-import GaragePage from "../pom/pages/GaragePage"
-import HomePage from "../pom/pages/HomePage";
-import SignInForm from "../pom/forms/SignInForm";
-import { EXISTING_USER } from "../test-data/users";
+
+import { test } from "../utils/app";
 import { faker } from "@faker-js/faker";
-import { afterEach } from "node:test";
-import EditCarForm from "../pom/forms/EditCarForm";
+import { CARS } from "../test-data/cars";
 
-let homePage: HomePage
-let signInForm: SignInForm;
-let garagePage: GaragePage;
-let addCarForm: AddCarForm;
-let editCarForm: EditCarForm;
 
-const cars = [
-    { brand: 'Audi', model: 'TT' },
-    { brand: 'BMW', model: '3' }
-]
+test.use({ storageState: './test-data/states/existingUserStorageState.json' });
 
 test.describe('Garage tests', () => {
-    test.beforeEach(async ({ page }) => {
-        garagePage = new GaragePage(page);
-        addCarForm = new AddCarForm(page);
-        editCarForm = new EditCarForm(page);
-        homePage = new HomePage(page);
-        signInForm = new SignInForm(page);
-        await page.goto('/');
-        await homePage.signInButton.click();
-        await signInForm.fillSingInForm(EXISTING_USER.email, EXISTING_USER.password);
-        await signInForm.signInButton.click();
+    test.beforeEach(async ({ app }) => {
+        await app.garagePage.openGaragePage();
     })
 
-    test.afterEach(async ({ }) => {
-        garagePage.editCarButton.click();
-        await editCarForm.removeCarButton.click();
-        await editCarForm.confirmRemoveButton.click();
+    test.afterEach(async ({ app }) => {
+        await app.garagePage.editCarButton.click();
+        await app.editCarForm.removeCarButton.click();
+        await app.editCarForm.confirmRemoveButton.click();
     })
 
-    // test('Add "Audi TT" to garage', async ({ page }) => {
-    //     await garagePage.addCarButton.click();
-    //     await addCarForm.addCar('Audi', 'TT', faker.number.int({ min: 1, max: 99999 }).toString());
-    // })
-
-    // test('Add "BMW 3" to garage', async ({ page }) => {
-    //     await garagePage.addCarButton.click();
-    //     await addCarForm.addCar('BMW', '3', faker.number.int({ min: 1, max: 99999 }).toString());
-    // })
-
-    cars.forEach(car => {
-        test(`Add "${car.brand} ${car.model}" to garage`, async ({ }) => {
-            await garagePage.addCarButton.click();
-            await addCarForm.addCar(car.brand, car.model, faker.number.int({ min: 1, max: 99999 }).toString());
+    CARS.forEach(car => {
+        test(`Add "${car.brand} ${car.model}" to garage`, async ({ app }) => {
+            await app.garagePage.addCarButton.click();
+            await app.addCarForm.addCar(car.brand, car.model, faker.number.int({ min: 1, max: 99999 }).toString());
         })
     });
 })
